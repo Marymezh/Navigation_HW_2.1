583bbef1c79a5411bc2f4388e86f2270fba90613
Marymezh <maria.mezhova@yahoo.com>
2021-05-19 08:43:26 +0700

    ProfileHeaderView and ProfileViewController were created in Profile group

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 19.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
